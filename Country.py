6be017fd8f48f941375f26825f37f51513f72e18
username <useremail>
2025-11-25 14:36:10 -0800

    New

--- Country.py
+++ Country.py
@@ -1,1 +1,25 @@
-#
+# O'Tria Fee
+# CIS261
+# Country
+
+def display_menu():
+    """
+    Function to display the heading and menu choices
+    """
+
+# Display "The Country List Program" heading
+print("The Country List Program")
+print()
+
+#Display "COMMAND MENU"
+print("COMMAND MENU")
+
+#Display menu options: view, add, del, exit with descriptions
+print("view- View a country")
+print("add- Add a country")
+print("del- Delete a country")
+print(exit- Exit program")
+print()
+
+
+
